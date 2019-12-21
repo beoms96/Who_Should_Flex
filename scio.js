0dd251f62da5966e9e8866ea39bd2b6bd84947c5
beoms <bm1478@naver.com>
2019-12-21 21:03:05 +0900

    complete simple wsf

---
+++ scio.js
@@ -0,0 +1,29 @@
+var socket;
+
+$(function () {
+    // socket.io 서버에 접속한다
+    socket = io.connect('http://localhost:3000');
+
+    // 접속 버튼이 클릭되면
+    $("form").submit(function (e) {
+        e.preventDefault();
+        var $msgForm = $("#msgForm");
+
+        // 서버로 id 전송한다.
+        socket.emit("login", {msg: $msgForm.val()});
+        $msgForm.val("");
+    });
+
+    // 서버로부터 로그인 수신되면
+    socket.on("login", function(data) {
+    $("#idLogs").empty();
+    for(var i=0; i<data.length; i++) {
+        $("#idLogs").append("<div><strong>" + data[i].id + "</strong></div>");
+    }
+});
+
+    // 서버로부터 결과 수신되면
+    socket.on("Result", function (data) {
+        // $("#idLogs").append("<div>" + data.msg + " : from <strong>" + data.from.name + "</strong></div>");
+    });
+});
